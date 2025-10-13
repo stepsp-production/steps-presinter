b3f337cf039fe7e31892c93832cb09acab3a11bc
stepsp-production <it@steps.net.sa>
2025-10-13 17:46:44 +0300

    Update ping.js

--- functions/vendor/ping.js
+++ functions/vendor/ping.js
@@ -1,4 +1,1 @@
-// functions/vendor/ping.js
-export async function onRequest() {
-  return new Response(null, { status: 204, headers: { 'Access-Control-Allow-Origin': '*' } });
-}
+export async function onRequest(){ return new Response(null,{status:204,headers:{'Access-Control-Allow-Origin':'*'}}); }

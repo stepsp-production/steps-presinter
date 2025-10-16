86865f06acd399cb4048071d4b73076f23074967
stepsp-production <it@steps.net.sa>
2025-10-16 20:36:09 +0300

    Update hls-player.js

--- vendor/hls-player.js
+++ vendor/hls-player.js
@@ -1,47 +1,31 @@
 // /vendor/hls-player.js
 (function () {
-  function attachHlsTo(videoEl, src) {
-    if (!window.Hls || !window.Hls.isSupported()) {
-      // Safari والأنظمة التي تدعم HLS أصلاً
-      videoEl.src = src;
-      videoEl.play().catch(() => {});
-      return () => {};
-    }
-
-    let hls = new window.Hls({
-      enableWorker: true,
-      lowLatencyMode: true,
-      manifestLoadingTimeOut: 15000,
-      fragLoadingTimeOut: 20000,
-      fragLoadingRetryDelay: 1000,
-      levelLoadingRetryDelay: 1000,
-    });
-
-    function onError(_, data) {
-      console.log('[HLS] ERROR', data);
-      if (data?.type === window.Hls.ErrorTypes.MEDIA_ERROR) {
-        try { hls.recoverMediaError(); } catch (e) {}
-        if (data?.details === window.Hls.ErrorDetails.FRAG_PARSING_ERROR) {
-          // إعادة التهيئة بدون Workers كحل أخير
+  window.HlsPlayer = {
+    attach(videoEl, src) {
+      if (!window.Hls || !window.Hls.isSupported()) {
+        videoEl.src = src;
+        videoEl.play?.().catch(()=>{});
+        return () => {};
+      }
+      let hls = new window.Hls({ enableWorker: true, lowLatencyMode: true });
+      function onErr(_, data){
+        console.log('[HLS] ERROR', data);
+        if (data?.type === window.Hls.ErrorTypes.MEDIA_ERROR &&
+            data?.details === window.Hls.ErrorDetails.FRAG_PARSING_ERROR) {
           try {
             hls.destroy();
             hls = new window.Hls({ enableWorker: false, lowLatencyMode: true });
-            hls.on(window.Hls.Events.ERROR, onError);
+            hls.on(window.Hls.Events.ERROR, onErr);
             hls.attachMedia(videoEl);
             hls.loadSource(src);
-          } catch (e) {}
+          } catch(e){}
         }
       }
+      hls.on(window.Hls.Events.ERROR, onErr);
+      hls.attachMedia(videoEl);
+      hls.loadSource(src);
+      videoEl.play?.().catch(()=>{});
+      return () => { try { hls.destroy(); } catch(e){} };
     }
-
-    hls.on(window.Hls.Events.ERROR, onError);
-
-    hls.attachMedia(videoEl);
-    hls.loadSource(src);
-    videoEl.play().catch(() => {});
-
-    return () => { try { hls.destroy(); } catch (e) {} };
-  }
-
-  window.attachHlsTo = attachHlsTo;
+  };
 })();

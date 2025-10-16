2e625be47f3ad2536f09c9d967863ae333b9d988
stepsp-production <it@steps.net.sa>
2025-10-16 20:12:41 +0300

    Update utils.js

--- vendor/utils.js
+++ vendor/utils.js
@@ -1,8 +1,8 @@
-// utils.js
-export function sleep(ms){ return new Promise(r=>setTimeout(r, ms)); }
-export function fmt(err) {
-  if (!err) return 'unknown';
-  if (typeof err === 'string') return err;
-  if (err.message) return err.message;
-  return String(err);
-}
+// /vendor/utils.js
+(function () {
+  function qs(sel, root) { return (root || document).querySelector(sel); }
+  function qsa(sel, root) { return Array.from((root || document).querySelectorAll(sel)); }
+  function wait(ms) { return new Promise(r => setTimeout(r, ms)); }
+
+  window.$utils = { qs, qsa, wait };
+})();
